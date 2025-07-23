6adeef8acd486af47ac71b8d97f92f135bdb8235
Malladi Sriram <malladisriram273@gmail.com>
2025-07-23 16:24:11 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,69 @@
+import streamlit as st
+import pickle
+import string
+import re
+from nltk.stem import PorterStemmer
+
+# Initialize stemmer
+ps = PorterStemmer()
+
+# Custom text transformation function without nltk downloads
+def transform_text(text):
+    # Lowercase
+    text = text.lower()
+
+    # Tokenize using regex
+    tokens = re.findall(r'\b\w+\b', text)
+
+    # Manually defined lightweight stopwords (to avoid downloading)
+    basic_stopwords = set([
+        'i', 'me', 'my', 'myself', 'we', 'our', 'ours', 'ourselves', 'you',
+        'your', 'yours', 'yourself', 'yourselves', 'he', 'him', 'his',
+        'himself', 'she', 'her', 'hers', 'herself', 'it', 'its', 'itself',
+        'they', 'them', 'their', 'theirs', 'themselves', 'what', 'which',
+        'who', 'whom', 'this', 'that', 'these', 'those', 'am', 'is', 'are',
+        'was', 'were', 'be', 'been', 'being', 'have', 'has', 'had', 'having',
+        'do', 'does', 'did', 'doing', 'a', 'an', 'the', 'and', 'but', 'if',
+        'or', 'because', 'as', 'until', 'while', 'of', 'at', 'by', 'for',
+        'with', 'about', 'against', 'between', 'into', 'through', 'during',
+        'before', 'after', 'above', 'below', 'to', 'from', 'up', 'down',
+        'in', 'out', 'on', 'off', 'over', 'under', 'again', 'further',
+        'then', 'once', 'here', 'there', 'when', 'where', 'why', 'how',
+        'all', 'any', 'both', 'each', 'few', 'more', 'most', 'other',
+        'some', 'such', 'no', 'nor', 'not', 'only', 'own', 'same', 'so',
+        'than', 'too', 'very', 'can', 'will', 'just', 'don', 'should',
+        'now'
+    ])
+
+    # Remove stopwords
+    tokens = [word for word in tokens if word not in basic_stopwords]
+
+    # Stemming
+    tokens = [ps.stem(word) for word in tokens]
+
+    return " ".join(tokens)
+
+# Load model and vectorizer
+tfidf = pickle.load(open('vectorizer.pkl', 'rb'))
+model = pickle.load(open('model.pkl', 'rb'))
+
+# Streamlit UI
+st.title("📩 SMS Spam Classifier")
+
+input_sms = st.text_area("Enter the message to classify:")
+
+if st.button('Predict'):
+    # Preprocess
+    transformed_sms = transform_text(input_sms)
+
+    # Vectorize
+    vector_input = tfidf.transform([transformed_sms])
+
+    # Predict
+    result = model.predict(vector_input)[0]
+
+    # Display result
+    if result == 1:
+        st.header("This SMS/ Message is a Spam !!!")
+    else:
+        st.header("This message is Not a Spam")
